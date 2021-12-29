44aace7a1a41cf0811353dbc8af6fa23d22c86ee
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-29 23:21:58 +0900

    Sign up

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.controller;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class MemberForm {
+
+  @NotEmpty(message = "name is required")
+  private String name;
+
+  private String city;
+  private String street;
+  private String zipcode;
+
+}
